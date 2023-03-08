7b93cc836640c0d8630e7327b48c451eb3acf21c
Abbos Ibrohimov <abbosibrohimov07@gmail.com>
2023-03-08 15:10:37 +0500

    First commit

---
+++ homework2/script.js
@@ -0,0 +1,11 @@
+let num = 90
+num <= 100 && num >= 86 && console.log('A');
+
+let ball = 80
+ball <= 85 && ball >= 61 && console.log('B');
+
+let number = 55
+number <= 60 && number >= 50 && console.log('C');
+
+let n = 45
+n < 50 && console.log('faild');
---
+++ homework4/script2.js
@@ -0,0 +1,6 @@
+let num = 10
+
+num % 2 == 0 && console.log('juft son');
+
+let number = 5
+num % 2 == 1 && console.log('toq son');
